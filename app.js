8db8c5885b4d8a9668ecc1ec1d00e61d2650ea93
kimmysoybad404 <6631501008@lamduan.mfu.ac.th>
2025-08-22 12:30:43 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+const con = require('./db');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// login service
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = "SELECT id, password FROM users WHERE username = ?";
+    con.query(sql, [username], function (err, results) {
+        if (err) {
+            return res.status(500).send("Database server error");
+        }
+        if (results.length != 1) {
+            return res.status(401).send("Wrong username");
+        }
+        // compare passwords
+        bcrypt.compare(password, results[0].password, function (err, same) {
+            if (err) {
+                return res.status(500).send("Hashing error");
+            }
+            if (same) {
+                return res.send("Login OK");   
+            }
+            return res.status(401).send("Wrong password");
+        });
+    })
+});
+
+//show expense service
+app.get('/expense', (_req, res) => {
+    const username = _req.query.username;
+    
+    const sql_id = "SELECT id FROM users WHERE username = ?";
+    con.query(sql_id, [username], function (err, userid) {
+        if (err) {
+            return res.status(500).send("Database server error");
+        }
+
+        if (userid.length === 0) {
+            return res.status(404).send("User not found.");
+        }
+
+        const sql = "SELECT * FROM expense WHERE user_id = ?";
+        con.query(sql, [userid[0].id], function (err, results) {
+            
+            if (err) {
+                return res.status(500).send("Database server error");
+            }
+            res.json(results);
+        })
+    })
+
+});
+
+// show today expense service
+app.get('/todayexpense', (_req, res) => {
+    const username = _req.query.username;
+    
+    const sql_id = "SELECT id FROM users WHERE username = ?";
+    con.query(sql_id, [username], function (err, userid) {
+        if (err) {
+            return res.status(500).send("Database server error");
+        }
+
+        if (userid.length === 0) {
+            return res.status(404).send("User not found.");
+        }
+
+        const sql = "SELECT * FROM expense WHERE user_id = ? AND DATE(date) = CURDATE();";
+        con.query(sql, [userid[0].id], function (err, results) {
+            
+            if (err) {
+                return res.status(500).send("Database server error");
+            }
+            res.json(results);
+        })
+    })
+
+});
+
+app.get('/searchexpense', (_req, res) => {
+    // search service
+});
+
+app.get('/addexpense', (_req, res) => {
+    // add service
+});
+
+app.get('/deleteexpense', (_req, res) => {
+    // delete service
+});
+
+
+
+// ---------- Server starts here ---------
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running at ' + PORT);
+});
